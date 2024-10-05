67b4693500ad50b1d27b9a5b92a689c980e9b117
Sonja Wong <sonja.hinting@gmail.com>
2024-10-05 18:02:16 -0400

    Added boxes to menu page.

--- components/HomeMenu/HomeMenu.tsx
+++ components/HomeMenu/HomeMenu.tsx
@@ -1,29 +1,78 @@
 import React from "react";
-import { View, Text, Image, StyleSheet } from "react-native";
-import buttons from "../../assets/images/buttons.png";
+import { View, Text, Image, StyleSheet, ScrollView, FlatList } from "react-native";
+import buttons from "assets/images/buttons.png";
 import heart from "../../assets/images/heart.svg";
 import home from "../../assets/images/home.svg";
 import icon from "../../assets/images/icon.svg";
 import image from "../../assets/images/image.svg";
 import liked from "../../assets/images/liked.svg";
+// var imageVar = require('../../assets/images/image.svg');
+
+const data = [
+  {
+    id: '1',
+    companyName: 'Company X',
+    imageSource: require('../../assets/images/icon.png'),
+  },
+  {
+    id: '2',
+    companyName: 'Company Y',
+    imageSource: require('../../assets/images/whitecrop.png'),
+  },
+  {
+    id: '3',
+    companyName: 'Company A',
+    imageSource: require('../../assets/images/white2.png'),
+  },
+  {
+    id: '4',
+    companyName: 'Company B',
+    imageSource: require('../../assets/images/blacktop.png'),
+  },
+  {
+    id: '5',
+    companyName: 'Company B',
+    imageSource: require('../../assets/images/white3.png'),
+  },
+  {
+    id: '6',
+    companyName: 'Company B',
+    imageSource: require('../../assets/images/white3.png'),
+  },
+];
 
 export const HomeMenu = (): JSX.Element => {
   return (
-    <View style={styles.homeMenu}>
+    // <ScrollView contentContainerStyle={styles.homeMenu}>
+      <View style={styles.homeMenu}>
       <View style={styles.div}>
         <View style={styles.overlap}>
           <View style={styles.body}>
             <View style={styles.overlapGroup}>
-              <View style={styles.overlap2}>
-                <Card />
-                <Card />
+              {/* <View style={styles.overlap2}>
+                <Card companyName="a" imageSource={require('../../assets/images/icon.png')}/>
+                <Card companyName="b" imageSource={require('../../assets/images/whitecrop.png')}/>
+                <Card companyName="c" imageSource={require('../../assets/images/white2.png')}/>
+                <Card companyName="d" imageSource={require('../../assets/images/blacktop.png')}/>
+            
                 <Image style={styles.icon} source={image} />
-              </View>
+              </View> */}
+              <FlatList
+                data={data}
+                renderItem={({ item }) => (
+                  <Card companyName={item.companyName} imageSource={item.imageSource} />
+                )}
+                keyExtractor={(item) => item.id}
+                numColumns={2}
+                columnWrapperStyle={styles.row}
+              />
             </View>
           </View>
         </View>
       </View>
     </View>
+    // </ScrollView>
+    
   );
 };
 
@@ -45,6 +94,12 @@ const styles = StyleSheet.create({
   },
   overlap2: {
     // Add your styles here
+    display: 'flex',          // Enables Flexbox layout
+    flexDirection: 'row',     // This makes the items go in a row (horizontal)
+    flexWrap: 'wrap',         // Allows items to wrap into multiple rows
+    justifyContent: 'center', // Centers the items horizontally
+    alignItems: 'center',     // Centers the items vertically (within their row)
+    width: '100%'
   },
   supportingText: {
     // Add your styles here
@@ -53,20 +108,38 @@ const styles = StyleSheet.create({
     // Add your styles here
   },
   card: {
-    backgroundColor: 'gray'
+    margin: 5,
+    width: '45%', 
+    alignItems: 'flex-start',     // Centers the content of each item horizontally
+    justifyContent: 'center', 
+    paddingLeft: 5,
+    overflow: 'hidden', 
   },
   cardImage: {
-    // Add your styles here
+    width: "100%",
+    height: undefined,
+    borderRadius: 20,
+    aspectRatio: 1,
+    resizeMode: 'contain',
   },
+
   cardText: {
-    alignItems: 'center'
+    alignItems: 'center',
+    backgroundColor: 'white',
+    fontWeight: 'bold',
+    margin: 5
   },
+  row: {
+    justifyContent: 'space-between',
+    marginBottom: 5,
+  }
 });
-export const Card = (): JSX.Element => {
+export const Card = ({ companyName, imageSource }: { companyName: string; imageSource: any }): JSX.Element => {
   return (
     <View style={styles.card}>
-      <Image style={styles.cardImage} source={icon} />
-      <Text style={styles.cardText}>Card Title</Text>
+      <Text style={styles.cardText}>@ {companyName}</Text>
+      <Image style={styles.cardImage} source={imageSource} />
+      
     </View>
   );
 };
